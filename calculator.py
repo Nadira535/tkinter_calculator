c76d1fb4bb85b2da3ad0a66b2b0e1bbcb1b324d8
Nadira Chithari <nadiracb66@gmail.com>
2020-07-15 10:15:52 +0300

    Colours Added

--- calculator.py
+++ calculator.py
@@ -2,4 +2,10 @@ from tkinter import*
 
 window=Tk()
 
+window.geometry("354x460")
+window.title("CALCULATOR")
+melabel = Label(window,text="CALCULATOR",bg='Blue',font=("Times",30,'bold'))
+melabel.pack(side=TOP)
+window.config(background='Dark Gray')
+
 window.mainloop()
